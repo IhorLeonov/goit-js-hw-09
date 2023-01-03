e958831dcca86725741fc46924a87fc7762ad76a
Ihor Leonov <ihorleonow@gmail.com>
2023-01-03 17:58:48 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+btnStop.setAttribute('disabled', 'true');
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+  this.setAttribute('disabled', 'true');
+  btnStop.removeAttribute('disabled');
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  this.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

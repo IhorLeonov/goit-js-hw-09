1f0cf77e3fc555268f7f453699721f15a1b9be29
Ihor Leonov <ihorleonow@gmail.com>
2023-01-06 14:39:19 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,37 +4,36 @@ const body = document.querySelector('body');
 
 btnStart.addEventListener('click', onBtnStartClick);
 btnStop.addEventListener('click', onBtnStopClick);
+let timerId;
 
 function onBtnStartClick() {
-  // btnStart.disabled = true;
-  // btnStop.disabled = false;
+  btnStart.disabled = true;
+  btnStop.disabled = false;
   timerId = setInterval(() => {
     const color = getRandomHexColor();
     body.style.backgroundColor = color;
   }, 1000);
-  switchBtn();
 }
 
 function onBtnStopClick() {
   clearInterval(timerId);
-  switchBtn();
-  // btnStart.disabled = false;
-  // btnStop.disabled = true;
+  btnStart.disabled = false;
+  btnStop.disabled = true;
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function switchBtn() {
-  if (!btnStart.disabled) {
-    btnStart.disabled = true;
-    btnStop.disabled = false;
-    return;
-  }
-  if (!btnStop.disabled) {
-    btnStop.disabled = true;
-    btnStart.disabled = false;
-    return;
-  }
-}
+// function switchBtn() {
+//   if (!btnStart.disabled) {
+//     btnStart.disabled = true;
+//     btnStop.disabled = false;
+//     return;
+//   }
+//   if (!btnStop.disabled) {
+//     btnStop.disabled = true;
+//     btnStart.disabled = false;
+//     return;
+//   }
+// }

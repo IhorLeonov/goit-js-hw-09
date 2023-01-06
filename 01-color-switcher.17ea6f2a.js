22f314df6950db1698ce31044d392dcc0010a37e
IhorLeonov <ihorleonow@gmail.com>
2023-01-06 12:39:47 +0000

    Deploying to gh-pages from @ IhorLeonov/goit-js-hw-09@1f0cf77e3fc555268f7f453699721f15a1b9be29 🚀

---
+++ 01-color-switcher.17ea6f2a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let d;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.17ea6f2a.js.map
